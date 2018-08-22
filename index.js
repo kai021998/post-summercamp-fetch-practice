b3ab319e61fa297862b8047cbfd8fa6c32285ea3
kai021998 <kai.han@balliol.ox.ac.uk>
2018-08-22 14:36:55 -0400

    done

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ const radioheadRemainingMembers = ["Jonny Greenwood", "Colin Greenwood", "Ed O'B
 
 const addMembersTrigger = document.getElementById('add-members-trigger')
 
-function addRemaningRadioheadMembers() {
+function postData(name) {
+  return fetch("http://localhost:3000/members", {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(
+      {
+        name: name,
+        band_id: 2,
+      }
+    )
+  })
+  .then(response => response.json())
+  .then(json => console.log(json.band_id))
+}
 
+function addRemaningRadioheadMembers() {
+  radioheadRemainingMembers.forEach(member => postData(member))
 }
 
 addMembersTrigger.addEventListener('click', addRemaningRadioheadMembers)
